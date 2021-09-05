91c65756ea26ac0ef71510844b8a2e80855ff019
Ronak98 <sonironak0906@gmail.com>
2021-09-05 23:49:36 +0530

    Checking CORS

--- src/Pages/Dashboard/dashboardAPI.js
+++ src/Pages/Dashboard/dashboardAPI.js
@@ -1,7 +1,7 @@
 // function to fetch ticker data
 export function fetchTickerData() {
   return new Promise((resolve) => {
-    fetch("https://api-pub.bitfinex.com/v2/ticker/tBTCUSD")
+    fetch("/v2/ticker/tBTCUSD")
       .then((res) => res.json())
       .then((data) => {
         resolve(data);
@@ -12,7 +12,7 @@ export function fetchTickerData() {
 // function to fetch order book data
 export function fetchOrderBookData() {
   return new Promise((resolve) => {
-    fetch("https://api-pub.bitfinex.com/v2/book/tBTCUSD/P0")
+    fetch("/v2/book/tBTCUSD/P0")
       .then((res) => res.json())
       .then((data) => {
         resolve(data);
